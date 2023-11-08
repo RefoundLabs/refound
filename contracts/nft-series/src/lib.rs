d76665bd10139ffca9e6e202608083c5c8b4a62b
genesisbarrios <genesisbarriosdev@gmail.com>
2023-11-08 17:40:27 +0000

    update contract metadata name

--- contracts/nft-series/src/lib.rs
+++ contracts/nft-series/src/lib.rs
@@ -128,8 +128,8 @@ impl Contract {
             owner_id,
             NFTContractMetadata {
                 spec: "nft-1.0.0".to_string(),
-                name: "NFT Series Contract".to_string(),
-                symbol: "GOTEAM".to_string(),
+                name: "Refound".to_string(),
+                symbol: "REFOUND".to_string(),
                 icon: None,
                 base_uri: None,
                 reference: None,
